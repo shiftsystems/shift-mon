1adc98ec52a05b28da4fbe0c7010abadb9f3e7ba
Mathias <mathias@mathiasp.me>
2020-08-08 05:00:11 +0000

    Update telegraf-configs/windows/example-install.ps1

---
+++ telegraf-configs/windows/example-install.ps1
@@ -0,0 +1,2 @@
+
+C:\users\Administrator\Documents\shift-rmm\telegraf.ps1 -telegraf_url "https://test.example.com" -telegraf-config-url "https://gitlab.com/shiftsystems/shift-rmm/-/raw/master/telegraf-configs/windows/telegraf.conf" -telegraf-org "shiftsystems" -telegraf-bucket "windows" -telegraf_version "1.15.2" -telegraf-token "GET_YOUR_OWN-TOKEN" 
