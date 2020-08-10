a5bf5e499b193898fe5053c07ce46b0f7f23f740
Mathias <mathias@mathiasp.me>
2020-08-10 05:55:01 +0000

    added mesh install script fixed #32

---
+++ telegraf-configs/windows/telegraf-mesh-install.ps1
@@ -0,0 +1,45 @@
+$telegraf_version="1.15.2"
+$telegraf_url="https://mon.example.com"
+$telegraf_org="muh_org"
+$telegraf_bucket="windows"
+$telegraf_token="GET_YOUR_OWN_KEY"
+
+$telegraf_env =("server=$telegraf_url","organization=$telegraf_org","token=$telegraf_token","bucket=$telegraf_bucket")
+if ((Test-Path 'C:\Program Files\telegraf') -and (&'C:\Program Files\telegraf\telegraf.exe' --version | Select-String "$telegraf_version" )) {
+    Write-Host "telegraf $telegraf_version is already installed now exiting"
+    exit
+}
+# download and zip telegraf
+$url = "https://dl.influxdata.com/telegraf/releases/telegraf-"+$telegraf_version+"_windows_amd64.zip"
+$output = "c:\windows\Temp\telegraf.zip"
+Invoke-WebRequest -Uri $url -OutFile $output
+
+# expand telegraf archive and create directories
+Expand-Archive 'C:\windows\Temp\telegraf.zip' 'C:\windows\Temp\' -Force
+Stop-Service telegraf
+
+if (Test-Path 'C:\Program Files\telegraf') {
+    Remove-Item 'C:\Program Files\telegraf' -Recurse -Force
+}
+Move-Item -Path "C:\windows\Temp\telegraf-$telegraf_version" -Destination "C:\Program Files\telegraf" -Force
+
+$path = "C:\Program Files\telegraf\conf\"
+If(!(test-path $path))
+{
+      New-Item -ItemType Directory -Force -Path $path
+}
+
+# download and install telegraf config
+$url = "https://gitlab.com/shiftsystems/shift-rmm/-/raw/master/telegraf-configs/windows/telegraf.conf"
+$output = "C:\Program Files\telegraf\telegraf.conf"
+Invoke-WebRequest -Uri $url -OutFile $output
+
+
+# Create and start telegraf service
+if (!(Get-Service telegraf)) {
+    & 'C:\Program Files\telegraf\telegraf.exe' --service install --config-directory 'C:\Program Files\telegraf\conf'
+}
+
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\telegraf"-Name "Environment" -PropertyType MultiString -Value $telegraf_env -Force
+
+Set-Service -Name telegraf -Status Running -StartupType Automatic
