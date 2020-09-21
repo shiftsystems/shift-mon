4e35e5f629fbb3a486ca9ded1de90e49f2671002
Mathias Palmersheim <mathias@mikasa.local.mathiasp.me>
2020-09-20 21:15:46 -0500

    fixed #52 will revist structed logs later

--- scripts/linux/telegraf-syslog.bash
+++ scripts/linux/telegraf-syslog.bash
@@ -1,12 +1,25 @@
 #!/bin/bash
-$SYSLOG_CONFIG_URL = "https://gitlab.com/shiftsystems/shift-rmm/-/raw/master/telegraf-configs/linux/50-telegraf.conf"
-$TELEGRAF_CONFIG_URL = "https://gitlab.com/shiftsystems/shift-rmm/-/raw/master/telegraf-configs/linux/telegraf-syslog.conf"
+SYSLOG_CONFIG_URL="https://gitlab.com/shiftsystems/shift-rmm/-/raw/master/telegraf-configs/linux/50-telegraf.conf"
+TELEGRAF_CONFIG_URL="https://gitlab.com/shiftsystems/shift-rmm/-/raw/master/telegraf-configs/linux/telegraf-syslog.conf"
+
+# install rsyslog
+source /etc/os-release
+
+case $ID in
+    "debian"|"ubuntu"|"neon")
+        apt install rsyslog
+        ;;
+    "fedora")
+        dnf install rsyslog
+        ;;
+
+esac
 
 # download syslog config
 curl -o /etc/rsyslog.d/50-telegraf.conf $SYSLOG_CONFIG_URL
 
 # download telegraf config
-curl -o /etc/telegraf.d/telegraf-syslog.conf $TELEGRAF_CONFIG_URL
+curl -o /etc/telegraf/telegraf.d/telegraf-syslog.conf $TELEGRAF_CONFIG_URL
 
 # restart telegraf and syslog
 systemctl restart telegraf rsyslog
