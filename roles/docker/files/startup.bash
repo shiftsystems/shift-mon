ed172f8e1426b1e008040e90e05520a06504f2a4
Mathias Palmersheim <mathias@shiftsystems.net>
2023-06-28 08:05:46 -0500

    switched command to update containers

--- roles/docker/files/startup.bash
+++ roles/docker/files/startup.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /opt/shift-mon
-docker pull $(grep image: docker-compose.yml | sed -e "s/image://" -e "s/ //g" | tr '\n' ' ')
+docker compose pull
 docker compose up -d
 echo "y" | docker system prune
